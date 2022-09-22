f55012cdaf01553778aa9e734f6b51b7e837a34b
Amir Hamza <hamza_amir@yahoo.com>
2022-09-22 21:04:53 +0600

    Part 001 - Introduction to C#

---
+++ Part_001_Introduction_To_C_Sharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main1()
+    {
+        Console.WriteLine("Welcome to C# Training 1");
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Welcome to C# Training");
+        Main1();
+        Console.ReadLine();
+    }
+}
