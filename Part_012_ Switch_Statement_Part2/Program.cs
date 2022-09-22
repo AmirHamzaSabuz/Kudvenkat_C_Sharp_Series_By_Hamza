b086accb283f14d4660fd60ffc76f0b8c2a90761
Amir Hamza <hamza_amir@yahoo.com>
2022-09-22 23:16:53 +0600

    Part 012 - Switch Statement Part 2

--- Part_012_ Switch_Statement_Part2/Program.cs
+++ Part_012_ Switch_Statement_Part2/Program.cs
@@ -49,3 +49,55 @@ Console.WriteLine("Thank you for shopping with us");
 Console.WriteLine("Bill amount = {0} ", totalCoffeeCost);
 
 Console.ReadKey();
+
+/*
+int totalCoffeeCost = 0;
+
+Start:
+int userChoice = 0;
+bool isUserChoiceInt;
+do
+{
+    Console.WriteLine("Select your coffee size: 1 for Small, 2 for Medium, 3 for Large. Write an integer form 1 to 3");
+    isUserChoiceInt = int.TryParse(Console.ReadLine(), out userChoice);
+} while (!isUserChoiceInt);
+
+
+switch (userChoice)
+{
+    case 1:
+        totalCoffeeCost = totalCoffeeCost + 1;
+        break;
+    case 2:
+        totalCoffeeCost = totalCoffeeCost + 2;
+        break;
+    case 3:
+        totalCoffeeCost = totalCoffeeCost + 3;
+        break;
+    default:
+        Console.WriteLine("Your Choice {0} is invalid", userChoice);
+        goto Start;
+}
+
+Decide:
+Console.WriteLine("Do you want to buy another coffee - Yes or No?");
+string? userDecision = Console.ReadLine();
+
+switch (userDecision.ToUpper())
+{
+    case "YES":
+        goto Start;
+    case "NO":
+        break;
+    default:
+        Console.WriteLine("Your choice {0} is invalid. Please try again", userDecision);
+        goto Decide;
+
+}
+
+Console.WriteLine("Thank you for shopping with us");
+Console.WriteLine("Bill amount = {0} ", totalCoffeeCost);
+
+
+Console.ReadKey();
+*/
