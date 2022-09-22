e94ee4e1f251e452c324be48acbddb5cb2a81d10
Amir Hamza <hamza_amir@yahoo.com>
2022-09-22 22:14:24 +0600

    Part 009 - Comments

---
+++ Part_009_ Comments/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Single line comments
+
+        /*
+         * Multi line comments
+         */
+
+        Customer customer = new Customer();
+        customer.Name = "Hamza";
+
+        Console.WriteLine(customer.Name);
+        Console.ReadKey();
+    }
+}
+
+/// <summary>
+/// Represents the properties of Customer
+/// </summary>
+public class Customer
+{
+    public string? Name { get; set; }
+
+}
