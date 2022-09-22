42cff8b6d9c2b918972b2ca0b2851d2ff40926b1
Amir Hamza <hamza_amir@yahoo.com>
2022-09-22 21:23:19 +0600

    Part 004 - string type

---
+++ Part_004_String_Type/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //Normal string
+        string strNormalString = "Normal String";
+        Console.WriteLine(strNormalString);
+
+        Console.WriteLine();
+
+        // Displaying double quotes in c#
+        string strDoubleQuotes1 = " \"Double\" \"Quotes\" ";
+        Console.WriteLine(strDoubleQuotes1);
+
+        Console.WriteLine();
+
+        string strDoubleQuotes2 = "\"Double\" \"Quotes\"";
+        Console.WriteLine(strDoubleQuotes2);
+
+        Console.WriteLine();
+
+        // Displaying new line character in c#
+        string strNewLineBreak = "Displaying\nNew\nLine\n";
+        Console.WriteLine(strNewLineBreak);
+
+        Console.WriteLine();
+
+        string strWithoutVerbatimLiteral = "C:\\DotNetWorkingSpace_dotnetcore6\\C# Kudvenkat\\Kudvenkat_C_Sharp_Series";
+        Console.WriteLine(strWithoutVerbatimLiteral);
+
+        Console.WriteLine();
+
+        string strVerbatimLiteral = @"C:\DotNetWorkingSpace_dotnetcore6\C# Kudvenkat\Kudvenkat_C_Sharp_Series";
+        Console.WriteLine(strVerbatimLiteral);
+
+        Console.WriteLine();
+
+        Console.ReadKey();
+    }
+}
