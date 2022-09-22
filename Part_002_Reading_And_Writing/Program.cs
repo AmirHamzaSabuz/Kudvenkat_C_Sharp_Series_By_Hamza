4443ae7532861848ba0e1c21fb8d7451ea6f3d6f
Amir Hamza <hamza_amir@yahoo.com>
2022-09-22 21:13:00 +0600

    Part 002 - Reading and writing

---
+++ Part_002_Reading_And_Writing/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your First name");
+        string? FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your last name");
+        string? LastName = Console.ReadLine();
+
+        Console.WriteLine("Hello {0} {1}", FirstName, LastName); // Placeholder Syntax
+        Console.WriteLine("Your full name is " + FirstName + " " + LastName); // Concatenation
+
+        Console.ReadKey();
+
+    }
+}
