a808202083b9f3a84dd7fe5547859ebc703caa84
Amir Hamza <hamza_amir@yahoo.com>
2022-09-23 17:56:32 +0600

    Part 019 - Introduction to class

---
+++ Part_019_Introduction_To_Classes/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Customer customer = new Customer(); 
+        Customer customer1 = new Customer("Amir", "Hamza");
+
+        customer.PrintFullName();
+        customer1.PrintFullName();
+
+        Console.ReadKey();
+    }
+}
+
+class Customer 
+{
+    string _firstName;
+    string _lastName;
+
+    //Constructor with parameter
+    public Customer(string firstName, string lastName) 
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+    }
+
+    //Constructor without parameter
+    public Customer() : this("No first name provided &", "No last name provided")
+    {
+    }
+
+    public void PrintFullName() 
+    {
+        Console.WriteLine("Full Name = {0} {1}", _firstName, _lastName);
+    }
+
+    ~Customer() //Destructor
+    {
+        //Clean up code
+    }
+}
