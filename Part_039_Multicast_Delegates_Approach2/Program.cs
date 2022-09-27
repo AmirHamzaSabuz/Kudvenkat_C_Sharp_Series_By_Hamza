ebeae45e5b745711c7486e7c5efa6d952aaa9d23
Amir Hamza <hamza_amir@yahoo.com>
2022-09-27 07:02:09 +0600

    Part 039 Multicast Delegates Approach2

---
+++ Part_039_Multicast_Delegates_Approach2/Program.cs
@@ -0,0 +1,43 @@
+﻿//Approach 2:
+
+//using System;
+namespace Part_039_Multicast_Delegates_Approach2
+{
+    public delegate void SampleDelegate();
+
+    public class Program
+    {
+        static void Main()
+        {
+            // In this example del is a multicast delegate. You use += operator 
+            // to chain delegates together and -= operator to remove.
+
+            SampleDelegate del = new SampleDelegate(SampleMethodOne);
+            del += SampleMethodTwo;
+            del += SampleMethodThree;
+            del -= SampleMethodTwo;
+
+            del();
+
+            Console.ReadKey();
+        }
+
+
+        public static void SampleMethodOne()
+        {
+            Console.WriteLine("SampleMethodOne Invoked");
+        }
+
+
+        public static void SampleMethodTwo()
+        {
+            Console.WriteLine("SampleMethodTwo Invoked");
+        }
+
+
+        public static void SampleMethodThree()
+        {
+            Console.WriteLine("SampleMethodThree Invoked");
+        }
+    }
+}
