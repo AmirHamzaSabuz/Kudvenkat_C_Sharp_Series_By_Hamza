b9a0c16bc87d0c4a7bae4f7c69b773fd42e257f2
Amir Hamza <hamza_amir@yahoo.com>
2022-09-27 17:23:56 +0600

    Part 042 - Custom Exceptions, 043 & 044 Exception hadling Abuse

---
+++ Part_042_Custom_Exceptions/CustomExceptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+public class CustomExceptions
+{
+    public static void Main()
+    {
+        try
+        {
+            throw new UserAlreadyLoggedInException("User Already logged in");
+        }
+        catch (UserAlreadyLoggedInException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
+[Serializable]
+public class UserAlreadyLoggedInException : Exception
+{
+    public UserAlreadyLoggedInException() : base()
+    {
+    }
+
+
+    public UserAlreadyLoggedInException(string message) : base(message)
+    {
+    }
+
+
+    public UserAlreadyLoggedInException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+    }
+}
---
+++ Part_044_Preventing_Exception_Handling_Abuse/PreventingExceptionHandlingAbuse.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+public class PreventingExceptionHandlingAbuse
+{
+    public static void Main()
+    {
+        try 
+        {
+            Console.WriteLine("Please enter Numerator");
+            int Numerator;
+
+            bool IsNumeratorConversionSuccesful = int.TryParse(Console.ReadLine(), out Numerator);
+
+            if (IsNumeratorConversionSuccesful)
+            {
+                Console.WriteLine("Please enter Denominator");
+                int Denominator;
+
+                bool IsDenominatorConversionSuccessful = int.TryParse(Console.ReadLine(), out Denominator);
+
+                if (IsDenominatorConversionSuccessful && Denominator != 0)
+                {
+                    int Result = Numerator / Denominator;
+                    Console.WriteLine("Result = {0}", Result);
+                }
+                else
+                {
+                    if (IsDenominatorConversionSuccessful && Denominator == 0)
+                    {
+                        Console.WriteLine("Denominator cannot be zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Only numbers between {0} && {1} are allowed",
+                            Int32.MinValue, int.MaxValue);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Only numbers between {0} && {1} are allowed", int.MinValue, Int32.MaxValue);
+            }
+        } // End try block
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    } // End Main method
+}
+
+
+/*
+int i = 12;
+// System.FormatException: 'Input string was not in a correct format.'
+//because { 0} // Space is not valid before 0
+Console.WriteLine("Hello { 0}", i); // Illegal, Error, Wrong code
+
+int a = 5;
+int b = 6;
+// System.FormatException: 'Input string was not in a correct format.'
+//because { 1} // Space is not valid before 1
+Console.WriteLine("Hello {0} { 1}", a, b); // Illegal, Error, Wrong code
+*/
+
