0dc85b1aefa197bfb1a91228d96d12a6931f8a9c
Amir Hamza <hamza_amir@yahoo.com>
2022-10-01 12:27:13 +0600

    Part 059 -  ToString() and Convert.ToString()

---
+++ Part_059_ToString_And_Convert_ToString/MainClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        Customer C1 = new Customer();
+        Console.WriteLine(C1.ToString());
+        //output: Customer
+
+
+        Console.WriteLine();
+
+        Customer C2 = null;
+
+        string str = Convert.ToString(C2);
+        // value of str is "". value of C2 is null
+
+        Console.WriteLine(str);
+
+        try
+        {
+             Console.WriteLine(C2.ToString()); // Null Reference Exception
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+public class Customer
+{
+    public string Name { get; set; }
+}
