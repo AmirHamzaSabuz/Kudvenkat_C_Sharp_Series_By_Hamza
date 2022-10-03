59fa179a6e30efe2eabe3ff7d471863a6741df64
Amir Hamza <hamza_amir@yahoo.com>
2022-10-03 11:57:45 +0600

    Part 068 - Optional Parameters using Method Overloading

---
+++ Part_068_Optional_Parameters_Method_Overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AddNumbers(10, 20, new int[] { 30, 40 });
+
+        AddNumbers(10, 20);
+
+
+    }
+    public static void AddNumbers(int firstNumber, int secondNumber)
+    {
+        AddNumbers(firstNumber, secondNumber, null);
+    }
+
+    public static void AddNumbers(int firstNumber, int secondNumber, int[] restOfNumbers)
+    {
+        int result = firstNumber + secondNumber;
+
+        if (restOfNumbers != null)
+        {
+            foreach (int i in restOfNumbers)
+            {
+                result += i;
+            }
+        }
+
+        Console.WriteLine("Result = " + result);
+    }
+}
