0cf16e1f038bc4dcc91e03c6179a9d3631bdb963
Amir Hamza <hamza_amir@yahoo.com>
2022-10-03 14:24:24 +0600

    Part 070 - Optional Parameters using OptionalAttribute

---
+++ Part_070_Optional_Parameters_OptionalAttribute/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AddNumbers1(10, 20, new int[] { 30, 40 });
+
+        AddNumbers1(10, 20);
+
+        AddNumbers2(10, 20, new int[] { 30, 40 });
+
+        AddNumbers2(10, 20);
+
+
+    }
+
+    public static void AddNumbers1(int firstNumber, int secondNumber, [Optional] int[] restOfNumbers)
+    {
+        int result = firstNumber + secondNumber;
+
+        if (restOfNumbers != null)
+        {
+            foreach (int i in restOfNumbers)
+            {
+                result += i;
+            }
+        }
+
+        Console.WriteLine("Result = " + result);
+    }
+
+    public static void AddNumbers2(int firstNumber, int secondNumber, [OptionalAttribute] int[] restOfNumbers)
+    {
+        int result = firstNumber + secondNumber;
+
+        if (restOfNumbers != null)
+        {
+            foreach (int i in restOfNumbers)
+            {
+                result += i;
+            }
+        }
+
+        Console.WriteLine("Result = " + result);
+    }
+}
