72ad619cf085771c41c5851fc59c8174d89f81ad
Amir Hamza <hamza_amir@yahoo.com>
2022-10-03 11:31:17 +0600

    Part 067 Optional Parameters using Parameter Array

---
+++ Part_067_Optional_Parameters_Parameter_Array/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AddNumbers(10, 20, new object[] { 30, 40, 50 });
+        AddNumbers(10, 20, 30, 40, 50); 
+    }
+
+    public static void AddNumbers(int firstNumber, int secondNumber, params object[] restOfNumbers)
+    {
+        int result = firstNumber + secondNumber;
+
+        if (restOfNumbers != null)
+        {
+            foreach (int i in restOfNumbers)
+            {
+                result += i;
+            }
+        }
+
+        Console.WriteLine("Result = " + result);
+    }
+}
